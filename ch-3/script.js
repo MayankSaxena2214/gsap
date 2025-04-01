71a97778998ffd1ed6d2e4bded9b755b58989d91
Mayank Saxena <mayanksaxena2214@gmail.com>
2025-04-01 17:58:21 +0530

    elastic wire

--- ch-3/script.js
+++ ch-3/script.js
@@ -1,13 +1,27 @@
-gsap.to("#page2 h1",{
-    transform:"translateX(-170%)",
-    scrollTrigger:{
-        trigger:"#page2",
-        scroller:"body",
-        markers:true,
-        start:"top 0%",
-        end:"top -100%",
-        scrub:2,
-        pin:true
+let path=`M 10 100 Q 500 100 990 100`;
+let finalPath=`M 10 100 Q 500 100 990 100`;
+ 
+let string=document.querySelector("#string");
 
-    }
+string.addEventListener("mousemove",function(dets){
+    path=`M 10 100 Q ${dets.x} ${dets.y} 990 100`
+    //now fit that into to the svg path using gsap
+    gsap.to("svg path",{
+        //to manipulate the attribute of an element
+        //we need to use attr and then in the objec
+        //use the key value pair . Key(attributte name)
+        //value ( attribute value)
+        attr:{d:path} ,
+        duration:0.3,
+        ease:"power3.out",
+    });
 });
+// again to the center position
+string.addEventListener("mouseleave",function(){
+    gsap.to("svg path",{
+        attr:{d:finalPath} ,
+        duration:1.5,
+        ease:"elastic.out(1,0.2)"
+        
+    });
+})
